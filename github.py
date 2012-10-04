bf80d53732453c5fb9c21348d151935d47560f42
Kanst <ae_jeka@mail.ru>
2012-10-04 17:49:08 +0400

    Убран ключ

--- github.py
+++ github.py
@@ -25,35 +25,13 @@ def main():
 	p(out)
 
 
-	c = subprocess.Popen('ls ~/.ssh', shell=True, stdout=subprocess.PIPE)
-	out = c.stdout.read()
-	p(out)
-	if 'id_rsa' in out:
-		c = subprocess.Popen('mkdir ~/.ssh/key_backup ', shell=True, stdout=subprocess.PIPE)
-		out = c.stdout.read()
-		p(out)
-
-		c = subprocess.Popen('cp ~/.ssh/id_rsa* ~/.ssh/key_backup && rm ~/.ssh/id_rsa*', shell=True, stdout=subprocess.PIPE)
-		out = c.stdout.read()
-		p(out)
-
-	#Creates a new ssh key using the provided email
-	ssh_up = 'ssh-keygen -t rsa -C "' + mail + '"'
-	c = subprocess.Popen(ssh_up, shell=True, stdout=subprocess.PIPE)
-	out = c.stdout.read()
-	p(out)
-	c = subprocess.Popen('apt-cache search xclip', shell=True, stdout=subprocess.PIPE)
-	out = c.stdout.read()
-	p(out)
+	
 
 	print "Если в следующем списке есть пакет xclip,\nто следует выполнить команду 'xclip -sel clip < ~/.ssh/id_rsa.pub'. После выполнения в буфере окажется публичный ключ. Иначе вначале нужно установить этот пакет (sudo apt-get install xclip)"
 	print "Далее следует проверить ключи (ssh -T git@github.com). Если вылетает ошибка, то можно выполнить 'ssh-add ~/.ssh/id_rsa'"
 
 
 
-	c = subprocess.Popen('ssh -T git@github.com', shell=True, stdout=subprocess.PIPE)
-	out = c.stdout.read()
-	p(out)
 
 
 if __name__ == '__main__':
